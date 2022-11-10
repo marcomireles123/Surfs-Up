205786f51f27b1f3b76228056191ff808a2af1e1
Marco <marco_mireles@outlook.com>
2022-11-10 15:42:40 -0600

    created stations route

--- app.py
+++ app.py
@@ -43,3 +43,7 @@ def precipitation():
    return jsonify(precip)
 
 @app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
