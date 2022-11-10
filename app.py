06ff15e386b50ef33798cc9aafe50bbdf0fd5fcc
Marco <marco_mireles@outlook.com>
2022-11-10 11:41:12 -0600

    imported new dependencies

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+# Import all dependencies
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
